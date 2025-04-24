a4a4d332c368b9b0ac862d15370ec60b3cf559e4
Aung Thu <dev104.ovmyanmar@gmail.com>
2025-04-24 11:23:43 +0630

    react context hook testing project

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,39 @@
+import React, { createContext, useReducer, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { bookReducer } from '../reducers/bookReducer';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    // using useState
+    // const [books, setBooks] = useState([
+    //     { title: 'test1', author: 'test1', id: 1 },
+    //     { title: 'test2', author: 'test2', id: 2 }
+    // ]);
+
+    //using useReducer
+
+    const [books, dispatch] = useReducer(bookReducer, []);
+
+    //using with useState
+    // const addBook = (title, author) => {
+    //     setBooks([...books, { title, author, id: uuidv4() }])
+    // };
+
+    // const removeBook = (id) => {
+    //     setBooks(books.filter(book => book.id !== id));
+    // }
+
+    return (
+        //using with useState
+        // <BookContext.Provider value={{ books, addBook, removeBook }}>
+        //     {props.children}
+        // </BookContext.Provider>
+
+        <BookContext.Provider value={{ books, dispatch }}>
+            {props.children}
+        </BookContext.Provider>
+    )
+}
+
+export default BookContextProvider;
